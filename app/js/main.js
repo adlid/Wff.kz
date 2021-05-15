f501612d5527225a1542b964dd7e790136abc1f6
adlid <baidildaevbahon08@gmail.com>
2021-05-15 19:59:24 +0600

    Fix burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -327,6 +327,7 @@ const iconMenu = document.querySelector('.menu__icon')
 const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
     iconMenu.addEventListener('click', (e) => {
+        console.log("OK")
         document.body.classList.toggle('_lock')
         iconMenu.classList.toggle('_active')
         menuBody.classList.toggle('_active')
